ea2b10a5d3592b896a9e74384aebf2c3b87004f0
parkerasnicar <parkerasnicar@gmail.com>
2024-09-12 20:41:18 +1000

    commit - dino game work

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
 function jump() {
     if (dino.classlist != "jump") {
@@ -10,7 +11,21 @@ function jump() {
     }
 }
 
+let isAlive = setInterval(function () {
 
+    // get current dino Y postion
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+    // get current cactus X position
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    // detect collision
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+
+        // collision
+        alert("Game Over!");
+    }
+}, 10);
 
 document.addEventListener("keydown", function (event) {
     jump();
